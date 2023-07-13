cdcb73acbfea287fc62c55a1d9c419fdd5934aeb
Lu Jae <songgy6666@gmail.com>
2023-07-13 19:53:09 +0800

    others

--- Line_notify+RFID+OLED(not complete).py
+++ Line_notify+RFID+OLED(not complete).py
@@ -1,14 +1,68 @@
+import machine
+from lib.dht import DHT11, InvalidChecksum
 from machine import I2C, Pin, UART, ADC
 import time
 from ssd1306 import SSD1306_I2C
 from mfrc522 import MFRC522
 from machine import Pin
 
+pin = Pin(28, Pin.OUT)
+sensor = DHT11(pin)
 g_led = Pin(25, Pin.OUT)
 r_led = Pin(14, Pin.OUT)
 
 ic = I2C(0, sda=Pin(12), scl=Pin(13), freq=400000)
 dis = SSD1306_I2C(128, 64, ic)
+
+wifi_ready=0
+uart = machine.UART(1,tx=Pin(8),rx=Pin(9),baudrate=115200)
+led = Pin(25,Pin.OUT)
+rst = Pin(5,Pin.OUT)
+rst.value(0)
+time.sleep(0.1)
+rst.value(1)
+reset='RESET'
+def sendCMD_waitResp(cmd, uart=uart, timeout=500):
+    print(cmd)
+    uart.write(cmd+'\r\n')
+    waitResp()
+   
+def waitResp(uart=uart, timeout=500):
+    global count,wifi_ready
+    prvMills = time.ticks_ms()
+    resp = b""
+    while (time.ticks_ms()-prvMills)<timeout:
+        if uart.any():
+            resp = b"".join([resp, uart.read(1)])
+    if resp != b'' :
+        resp = str(resp)  
+        print(resp)   #印出接收MQTT的paylod
+        if (resp.find('connect'))>=0:
+            print('Ready')
+            wifi_ready=1
+            
+sendCMD_waitResp("RESET")
+time.sleep(0.5)
+sendCMD_waitResp("SSID+lu's project")
+time.sleep(0.1)
+sendCMD_waitResp("PSWD+songgy0926")
+time.sleep(0.1)
+sendCMD_waitResp("TOKEN+ZTFrMwibjoJ26tmaceTLNzhXWwPpojFj684ORdSrhF1")
+time.sleep(0.1)
+sendCMD_waitResp("ready")
+time.sleep(0.1)
+
+while (not wifi_ready) :
+    time.sleep(0.3)
+    led.value(1)
+    print('.')
+    time.sleep(0.3)
+    led.value(0)
+    print('.')
+    waitResp()    
+print('start')
+time.sleep(5)
+
 def uidToString(uid):
     mystring = ""
     for c in uid:
@@ -16,7 +70,7 @@ def uidToString(uid):
     return mystring
 
 reader = MFRC522(spi_id=0, sck=2, miso=4, mosi=3, cs=26, rst=10)
-print(".....card to sensor please.....")
+print(".....commodity to sensor.....")
 
 try:
     while True:
@@ -27,15 +81,17 @@ try:
                 card_num = uidToString(uid)
                 print("...card num: %s" % card_num)
                 if card_num == "A9968823":
-                    print('商品1')
+                    print('commodity1')
                     g_led.value(1)
                     time.sleep(1)
                     g_led.value(0)
+                    #sendCMD_waitResp('commodity coming')
+                    time.sleep(0.5)
                     dis.text(card_num,10,10)
                     dis.text("commodity1",10,25)
                     dis.text("kind:",4,50)
                 elif card_num == "A59D7953":
-                    print("商品2")
+                    print("commodity2")
                     g_led.value(1)
                     time.sleep(2)
                     g_led.value(0)
@@ -43,7 +99,7 @@ try:
                     dis.text("commodity2",10,25)
                     dis.text("kind:",4,50)
                 elif card_num == "A62812C3":
-                    print("商品3")
+                    print("commodity3")
                     g_led.value(1)
                     time.sleep(2)
                     g_led.value(0)
@@ -51,7 +107,7 @@ try:
                     dis.text("commodity3",10,25)
                     dis.text("kind:",4,50)
                 elif card_num == "A6FF1433":
-                    print("商品4")
+                    print("commodity4")
                     g_led.value(1)
                     time.sleep(2)
                     g_led.value(0)
@@ -59,7 +115,7 @@ try:
                     dis.text("commodity4",10,25)
                     dis.text("kind:",4,50)
                 elif card_num == "A62703C3":
-                    print("商品5")
+                    print("commodity5")
                     g_led.value(1)
                     time.sleep(2)
                     g_led.value(0)
@@ -67,7 +123,7 @@ try:
                     dis.text("commodity5",10,25)
                     dis.text("kind:",4,50)
                 elif card_num == "A6669663":
-                    print("商品6")
+                    print("commodity6")
                     g_led.value(1)
                     time.sleep(2)
                     g_led.value(0)
@@ -75,9 +131,9 @@ try:
                     dis.text("commodity6",10,25)
                     dis.text("kind:",4,50)
                 else:
-                    print("查無此項")
+                    print("Out of range")
                     r_led.value(1)    # 讀到除其他商品以外的物件後點亮紅LED
-                    time.sleep(2)    # 亮 2 秒鐘
+                    time.sleep(1)    # 亮1秒鐘
                     r_led.value(0)
                     dis.text(card_num,10,10)
                     dis.text("???",10,25)
--- 網頁/script.js
+++ 網頁/script.js
@@ -1,14 +1,13 @@
-document.write("<h1>主題:RFID結合商運系統</h1>");
-document.write("主旨1<br>");
-document.write("起因2<br>");
-document.write("材料3<br>");
-document.write("運作原理4<br>");
-document.write("過程5<br>");
-document.write("問題處理6<br>");
-document.write("成果7<br>");
-document.write("結論8<br>");
-document.write("謝幕9<br>");
-document.write("END<br>");
+document.write("<h1>主題:RFID結合AI自動商運&檢貨系統</h1>");
+document.write("1.起因<br>");
+document.write("2.材料<br>");
+document.write("3.運作原理<br>");
+document.write("4.過程<br>");
+document.write("5.問題處理<br>");
+document.write("6.成果發表<br>");
+document.write("7.結論<br>");
+document.write("8.討論未來發展<br>");
+document.write("9.END<br>");
 
 var my_name = "LUJE"
 document.write("The producer is" + my_name);
