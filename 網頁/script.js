958f6ba6f514c7877099efe2f0010eb7ec53d304
Lu Jae <songgy6666@gmail.com>
2023-07-13 17:06:21 +0800

    RE

---
+++ 網頁/script.js
@@ -0,0 +1,14 @@
+document.write("<h1>主題:RFID結合商運系統</h1>");
+document.write("主旨1<br>");
+document.write("起因2<br>");
+document.write("材料3<br>");
+document.write("運作原理4<br>");
+document.write("過程5<br>");
+document.write("問題處理6<br>");
+document.write("成果7<br>");
+document.write("結論8<br>");
+document.write("謝幕9<br>");
+document.write("END<br>");
+
+var my_name = "LUJE"
+document.write("The producer is" + my_name);
