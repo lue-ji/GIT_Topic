0dce46520749fb9c40f49bda363dc4d286f4effe
Lu Jae <songgy6666@gmail.com>
2023-07-16 17:59:13 +0800

    try

---
+++ 網頁X2nd/another.js
@@ -0,0 +1,16 @@
+let title = document.getElementById("title");
+let content = document.getElementById("content");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+
+btn.addEventListener("click", function(){
+    list.innerHtml = list.innerHTML + `
+    <div class="articale">
+        <h2>${title.value}</h2>
+        <p>${content.value}</p>
+    </div>
+    `;
+    title.value = "";
+    content.value = "";
+    
+})
---
+++ 網頁X2nd/script.js
@@ -0,0 +1,25 @@
+document.write("<h1>主題:RFID結合AI自動貨運&檢貨系統</h1>");
+document.write("1.起因<br>");
+document.write("2.材料<br>");
+document.write("3.運作原理<br>");
+document.write("4.過程<br>");
+document.write("5.問題處理<br>");
+document.write("6.成果發表<br>");
+document.write("7.結論<br>");
+document.write("8.討論未來發展<br>");
+document.write("9.END<br>");
+
+var my_name = "LUJE"
+document.write("The producer is" + my_name);
+
+//function warning(element){
+    //element.innertext = "";
+   // element.style.color = "blue";
+    //alert("there is no secret")
+//}
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    this.innertext = "APE"
+    this.style.color = "blue"
+    alert("there is no secret")
+})
