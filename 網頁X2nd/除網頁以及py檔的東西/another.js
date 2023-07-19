1792bd25c7f018fc27694be8b3fae477e6778720
Lu Jae <songgy6666@gmail.com>
2023-07-19 12:45:51 +0800

    更改網頁部分

--- 網頁X2nd/除網頁以及py檔的東西/another.js
+++ 網頁X2nd/除網頁以及py檔的東西/another.js
@@ -4,7 +4,7 @@ let btn = document.getElementById("btn");
 let list = document.getElementById("list");
 
 btn.addEventListener("click", function(){
-    list.innerHtml = list.innerHTML + `
+    list.innerHTML += `
     <div class="articale">
         <h2>${title.value}</h2>
         <p>${content.value}</p>
